2697e4bdf0847a85201f38d57759e83f49b88726
Sukhdip Singh Nijjar <sukhdipsinghnijjar@Sukhdips-MacBook-Pro-2.local>
2020-06-05 11:20:19 -0700

    adding stock model and api gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_223123) do
+ActiveRecord::Schema.define(version: 2020_06_05_181353) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.string "name"
+    t.decimal "last_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
